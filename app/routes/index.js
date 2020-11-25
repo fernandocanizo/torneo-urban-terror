fdbbd585825ff86e7347fa6a5d2edfdbe16064fb
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2020-11-25 19:46:22 -0300

    User register form stub

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,9 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 
+const siteTitle = 'Torneo Urban Terror Latinoamericano';
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Torneo Urban Terror Latinoamericano' });
+  res.render('index', { title: siteTitle });
 });
 
+router.get('/register', (req, res) => {
+  res.render('register', { title: siteTitle });
+});
+
+router.post('/register-user', (req, res) => {
+  console.log(req.body);
+  res.send('No implementado');
+});
 
 module.exports = router;
