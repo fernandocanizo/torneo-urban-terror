3df1ddeed924b5f2e214f07ab3efec317fb7c1fb
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2020-11-27 12:23:58 -0300

    Fix bad English

--- app/lib/verifyJwt.js
+++ app/lib/verifyJwt.js
@@ -20,7 +20,7 @@ const urlExceptions = [
 const verifyJwt = (req, res, next) => {
   console.log('---->', req.originalUrl)
   if (urlExceptions.find(url => url === req.originalUrl.replace(/(.+)\/$/, '$1'))) {
-    console.debug(`${req.originalUrl} not needs authentication`);
+    console.debug(`${req.originalUrl} doesn't need authentication`);
     return next();
   }
 
